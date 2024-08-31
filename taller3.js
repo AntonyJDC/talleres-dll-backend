79483462f340397aedb788961330f98b6718c99c
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-08-31 00:21:09 -0500

    Punto 3

--- taller3.js
+++ taller3.js
@@ -26,3 +26,31 @@ function twoSum(lista, numero){
     }
     return resultado;
 }
+
+function conversionRomana(str){
+    const romanos = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    };
+
+    let resultado = 0;
+    let anterior = 0;
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        const actual = romanos[str.charAt(i)];
+        if (actual < anterior) {
+            resultado -= actual;
+        } else {
+            resultado += actual;
+        }
+        anterior = actual;
+    }
+
+    return resultado;
+}
+
