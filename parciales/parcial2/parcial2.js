44d863c3ca1787ac62d3a518d3165820af43b969
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-09-13 23:50:16 -0500

    Parcial 2 - 2do punto

--- parciales/parcial2/parcial2.js
+++ parciales/parcial2/parcial2.js
@@ -7,3 +7,25 @@ function puntoUno(estudiantes) {
   return estudiantesInnova.map((estudiante) => estudiante.info_personal.correo);
 
 }
+
+function puntoDos(estudiantes) {
+  return Object.values(
+      estudiantes.map(est => {
+      const maxSemestre = Math.max(...est.info_matricula.map(mat => mat.semestre));
+
+      const materiasMaxSemestre = est.info_matricula.filter(mat => mat.semestre === maxSemestre);
+
+      const promedio = materiasMaxSemestre
+        .map(mat => mat.notas.reduce((acc, nota) => acc + nota.nota * nota.peso, 0))
+        .reduce((acc, sum) => acc + sum, 0) / materiasMaxSemestre.length;
+
+      return { nombreCompleto: `${est.info_personal.nombre} ${est.info_personal.apellido}`, semestre: maxSemestre, promedio };
+    })
+    .reduce((acc, est) => {
+      if (!acc[est.semestre] || acc[est.semestre].promedio < est.promedio) {
+        acc[est.semestre] = est;
+      }
+      return acc;
+    }, {}))
+    .map(est => est.nombreCompleto);
+}
