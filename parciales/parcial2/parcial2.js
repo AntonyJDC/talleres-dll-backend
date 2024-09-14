9ef34cb2de3804be0d09ea0bc2ac7bd9389dcde8
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-09-13 23:50:36 -0500

    Parcial 2 - 3er punto

--- parciales/parcial2/parcial2.js
+++ parciales/parcial2/parcial2.js
@@ -29,3 +29,22 @@ function puntoDos(estudiantes) {
     }, {}))
     .map(est => est.nombreCompleto);
 }
+
+function puntoTres(estudiantes) {
+  const estudiantesInfo = estudiantes
+    .filter(estudiante => estudiante.info_matricula.every(curso => curso.semestre === 1))
+    .map(estudiante => ({
+      gender: estudiante.info_personal.gender,
+      titulo: estudiante.info_personal.gender === "M" ? "Sr." : "Sra.",
+      nombreCompleto: `${estudiante.info_personal.nombre} ${estudiante.info_personal.apellido}`,
+      primerNombre: estudiante.info_personal.nombre,
+      primerApellido: estudiante.info_personal.apellido,
+      altura: estudiante.info_personal.altura * 100,
+      edad: estudiante.info_personal.nacimiento ? new Date().getFullYear() - new Date(estudiante.info_personal.nacimiento).getFullYear() : null,
+      nacimiento: estudiante.info_personal.nacimiento,
+      correo: estudiante.info_personal.correo,
+      usuario: estudiante.info_personal.correo.split("@")[0],
+    }));
+
+  return estudiantesInfo;
+}
