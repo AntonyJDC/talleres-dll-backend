81090f7df3c03a702bc0c323e50e92b9a96acf9c
Antony Dominguez <jaconsuegra@uninorte.edu.co>
2024-09-14 00:02:21 -0500

    Parcial 1 - 1er punto

---
+++ parciales/parcial1/parcial1.js
@@ -0,0 +1,38 @@
+const datos = require("./datos.json");
+
+/*
+  Para correr este archivo, utilicen el comando
+  $ node parcial1.js
+
+  Para abrir el terminal desde la ubicacion de este folder, usen Ctrl + Shift + C.
+
+  La variable importada "datos" contiene datos de estudiantes.
+*/
+
+/**
+ * Implemente una funcion que reciba los datos de estudiantes y
+ * retorne la cantidad de estudiantes en matricula condicional
+ * (promedio por debajo de 3.3).
+ *
+ * Los 'estudiantes' vienen del archivo 'datos.json', que ya fue importado para ustedes.
+ */
+function puntoUno(estudiantes) {
+  let estudiantesMatriculaCondicional = 0;
+
+  for (let i = 0; i < estudiantes.length; i++) {
+      let estudiante = estudiantes[i];
+      let totalNotas = 0;
+
+      for (let j = 0; j < estudiante.cursos.length; j++) {
+          totalNotas += estudiante.cursos[j].nota;
+      }
+
+      let promedio = totalNotas / estudiante.cursos.length;
+
+      if (promedio < 3.3) {
+          estudiantesMatriculaCondicional++;
+      }
+  }
+
+  return estudiantesMatriculaCondicional;
+}
